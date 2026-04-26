3af0b3290fb9156adcf4ce13c989f323b24053d2
Mapelujo Abdulkareem <olamideiyanda18@gmail.com>
2026-04-26 16:00:27 +0100

    initial commit

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,31 @@
+using CityInfo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo
+{
+    [Route("api/cities")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            // find the city
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
