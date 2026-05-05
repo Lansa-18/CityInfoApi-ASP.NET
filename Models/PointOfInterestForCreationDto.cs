0fbdb39f62d727d3161fd9be951cbb1c2a71d321
Mapelujo Abdulkareem <olamideiyanda18@gmail.com>
2026-05-05 12:24:05 +0100

    creating a resource

---
+++ Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CityInfo.Models;
+
+public class PointOfInterestForCreationDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
