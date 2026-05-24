5918eabbcc1737f0b7ac02c2b6f418fcd1588bcc
Mapelujo Abdulkareem <olamideiyanda18@gmail.com>
2026-05-24 19:11:31 +0100

    work with configuration files

--- Services/LocalMailService.cs
+++ Services/LocalMailService.cs
@@ -4,8 +4,14 @@ namespace CityInfo;
 
 public class LocalMailService : IMailService
 {
-    private string _mailTo = "admin@mycompany.com";
-    private string _mailFrom = "noreply@mycompany.com";
+    private string _mailTo = string.Empty;
+    private string _mailFrom = string.Empty;
+
+    public LocalMailService(IConfiguration configuration)
+    {
+        _mailTo = configuration["mailSettings:mailToAddress"];
+        _mailFrom =  configuration["mailSettings:mailFromAddress"];
+    }
 
     public void Send(string subject, string message)
     {
